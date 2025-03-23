b7570b1fcfd7ae4eee4633592aba2cfe169985da
Manasa-63057 <manasa63057@gmail.com>
2025-03-23 18:11:59 +0530

    Add files via upload

---
+++ fraud.bat
@@ -0,0 +1,17 @@
+pip install numpy==1.18.5
+pip install matplotlib==3.1.1
+pip install h5py==2.10.0
+pip install scikit-learn==0.22.2.post1
+pip install h5py==2.10.0 
+pip install protobuf==3.19.0
+pip install mysqlclient==1.4.6
+pip install opencv-python==4.2.0.32
+Pip install opencv-contrib-python==4.3.0.36
+pip install django==2.1.7
+pip install pymysql==0.9.3
+pip install openpyxl==3.1.2
+pip install xlwt==1.3.0
+pause
+
+
+
